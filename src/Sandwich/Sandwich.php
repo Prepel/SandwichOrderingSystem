44a20f7276c8e4ffd134b4497bcfda3b295372fe
Nick Slabbekoorn <nick@yoursurprise.com>
2017-03-31 16:34:01 +0200

    Added Sandwich

---
+++ src/Sandwich/Sandwich.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Sandwich;
+
+class Sandwich
+{
+    /** @var  Type */
+    private $type;
+    /** @var  Size */
+    private $size;
+    /** @var  Flavour */
+    private $flavour;
+
+    /**
+     * Sandwich constructor.
+     *
+     * @param Type $type
+     * @param Size $size
+     * @param Flavour $flavour
+     */
+    public function __construct( Type $type, Size $size, Flavour $flavour )
+    {
+        $this->type    = $type;
+        $this->size    = $size;
+        $this->flavour = $flavour;
+    }
+
+    /**
+     * @return Type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return Size
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @return Flavour
+     */
+    public function getFlavour()
+    {
+        return $this->flavour;
+    }
+
+}
