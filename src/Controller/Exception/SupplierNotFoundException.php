7629bcb32635fca29a33d193b43751e7e6180543
Nick Slabbekoorn <nick@yoursurprise.com>
2017-04-28 15:39:18 +0200

    Added supplier controller

---
+++ src/Controller/Exception/SupplierNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Controller\Exception;
+
+class SupplierNotFoundException extends \Exception
+{
+
+}
---
+++ src/Controller/Supplier/SupplierController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Controller\Supplier;
+
+use Controller\Sandwich\SandwichController;
+use Dao\Dao;
+use Domain\Supplier\Supplier;
+use Domain\Util\Email;
+use Domain\Util\Name;
+use Domain\Util\PhoneNumber;
+
+class SupplierController
+{
+
+    public function getSuppliers()
+    {
+        $dao = new Dao();
+        $suppliersData = $dao->getSuppliers();
+
+        return $this->createSuppliersFromDatabaseData($suppliersData);
+    }
+
+    /**
+     * @param int $supplierId
+     *
+     * @return Supplier
+     */
+    public function getSupplierById( $supplierId )
+    {
+        $dao          = new Dao();
+        $supplierData = $dao->getSupplierById( $supplierId );
+
+        if (!$supplierData) {
+            throw new SupplierNotFoundException( 'Supplier with id: ' . $id . ' can not be found.' );
+        }
+
+        return $this->createSupplierFromDatabaseData( $supplierData );
+    }
+
+    /**
+     * @param array $suppliersData
+     *
+     * @return Supplier[]
+     */
+    private function createSuppliersFromDatabaseData($suppliersData)
+    {
+        $suppliers = [];
+        foreach($suppliersData as $supplierData) {
+            $suppliers[] = $this->createsupplierFromDatabaseData($supplierData);
+        }
+
+        return $suppliers;
+    }
+
+    /**
+     * @param array $supplierData
+     *
+     * @return Supplier
+     */
+    private function createsupplierFromDatabaseData( $supplierData )
+    {
+        $sandwichController = new SandwichController();
+        $toppedSandwiches   = $sandwichController->getActiveToppedSandwichesForSupplier( $supplierData[ 'id' ] );
+
+        return new Supplier(
+            $supplierData[ 'id' ],
+            new Name( $supplierData[ 'name' ] ),
+            new Email( $supplierData[ 'email' ] ),
+            new PhoneNumber( $supplierData[ 'phone_number' ] ),
+            $toppedSandwiches
+        );
+    }
+}
